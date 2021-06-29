2a29d243a180d7b9ef581c095b58d25146008782
Kaname21Miura <miukana21@gmail.com>
2021-06-29 21:16:52 +0900

    utilsにcorrelationLineを追加

--- pymopt/utils/__init__.py
+++ pymopt/utils/__init__.py
@@ -2,7 +2,7 @@
 from .validation import _deprecate_positional_args
 from .montecalro import MonteCalro
 from .readDICOM import readDicom,reConstArray_8,reConstArray
-from .utilities import calTime,set_params,ToJsonEncoder
+from .utilities import calTime,set_params,ToJsonEncoder,correlationLine
 
 __all__ = [
     '_deprecate_positional_args',
@@ -12,5 +12,6 @@ __all__ = [
     'reConstArray_8',
     'reConstArray',
     'calTime','set_params',
-    'ToJsonEncoder'
+    'ToJsonEncoder',
+    'correlationLine',
     ]
